6596dde82f7131b7a3bdcc5d991a8e7dbcdd47c5
sfrfln <nadiasafa0505@gmail.com>
2026-03-26 00:57:03 +0700

    Update core_mcu.cpp

--- soal/soal_1_oop/core_mcu.cpp
+++ soal/soal_1_oop/core_mcu.cpp
@@ -1,27 +1,3 @@
 #include "core_mcu.hpp"
-Core_MCU::Core_MCU(float cpu_speed,int memory,string os,string nama,int volt){
-this->cpu_speed = cpu_speed;
-this->memory = memory;
-this->os = os;
-this->nama = nama;
-this->volt = volt;
-
-cout << "[Core MCU] : MCU utama yang bernama " << nama << " telah dibuat" << endl;
-}
-void Core_MCU::showSpek(){
-    cout << "[SPEK DEVICE] : [NAMA DEVICE] : " << nama << " | [CPU CLOCK] : " << cpu_speed << " | [MEMORY] : " << memory << " | [OS] : " << os << endl;
-}
-string Core_MCU::get_os(){
-    return os;
-}
-void Core_MCU::nambah_volt(int penambahan_voltase){
-    volt += penambahan_voltase;
-    cout << "[Core MCU] : MCU utama yang bernama " << nama << " Telah ditambah voltasenya menjadi = " << volt << endl;
-}
-void Core_MCU::ganti_os(string new_os){
-    os = new_os;
-    cout << "[Core MCU] : MCU utama yang bernama " << nama << " Telah diganti OS nya menjadi = " << os << endl;
-}
-Core_MCU::~Core_MCU(){
-    cout << "[Core MCU] : MCU utama yang bernama " << nama << " telah dihapus" << endl;
-}
+// [TODO]: Implementasikan class robot di file cpp ini
+// [TIPS]: Gunakan CoreMCU:: <nama methode> untuk mengimplementasikan methodenya
