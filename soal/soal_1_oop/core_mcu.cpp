df9f31314cc3335e6cca15647152ec5910ef339b
sfrfln <nadiasafa0505@gmail.com>
2026-03-26 00:08:05 +0700

    Update core_mcu.cpp

--- soal/soal_1_oop/core_mcu.cpp
+++ soal/soal_1_oop/core_mcu.cpp
@@ -1,12 +1,12 @@
 #include "core_mcu.hpp"
-Core_MCU(float cpu_speed,int memory,string os,string nama,int volt){
-cpu_speed = cpu;
-memory = mem;
-os = os1;
-nama = nama_device;
-volt = volt1;
+Core_MCU::Core_MCU(float cpu_speed,int memory,string os,string nama,int volt){
+this->cpu_speed = cpu_speed;
+this->memory = memory;
+this->os = os;
+this->nama = nama;
+this->volt = volt;
 
-cout << "[Core MCU] : MCU utama yang bernama" << nama << " telah dibuat" << endl;
+cout << "[Core MCU] : MCU utama yang bernama " << nama << " telah dibuat" << endl;
 }
 void Core_MCU::showSpek(){
     cout << "[SPEK DEVICE] : [NAMA DEVICE] : " << nama << " | [CPU CLOCK] : " << cpu_speed << " | [MEMORY] : " << memory << " | [OS] : " << os << endl;
@@ -16,11 +16,11 @@ string Core_MCU::get_os(){
 }
 void Core_MCU::nambah_volt(int penambahan_voltase){
     volt += penambahan_voltase;
-    cout << "[Core MCU] : MCU utama yang bernama" << nama << " Telah ditambah voltasenya menjadi = " << volt << endl;
+    cout << "[Core MCU] : MCU utama yang bernama " << nama << " Telah ditambah voltasenya menjadi = " << volt << endl;
 }
 void Core_MCU::ganti_os(string new_os){
     os = new_os;
-    cout << "[Core MCU] : MCU utama yang bernama" << nama << " Telah diganti OS nya menjadi = " << os << endl;
+    cout << "[Core MCU] : MCU utama yang bernama " << nama << " Telah diganti OS nya menjadi = " << os << endl;
 }
 Core_MCU::~Core_MCU(){
     cout << "[Core MCU] : MCU utama yang bernama " << nama << " telah dihapus" << endl;
