84c538444a128db9dbdbbd08f808595a595d1327
sfrfln <nadiasafa0505@gmail.com>
2026-03-26 00:50:29 +0700

    Create jawaban core_mcu.hpp

---
+++ src/oop_soal_1/jawaban core_mcu.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "bits/stdc++.h"
+using namespace std;
+
+/*[TODO]: Implementasikanlah class berikut didalam file robot.cpp agar main.cpp dapat dijalankan */
+class Core_MCU{
+private:
+    float cpu_clock;
+    int memory;
+    string os;
+    string nama;
+    int volt:
+public:
+    Core_MCU(float cpu_speed,int memory,string os,string nama,int volt);
+    void showSpek();
+    ~Core_MCU();
+    void nambah_volt(int penambahan_voltase);
+    void ganti_os(string new_os);
+    string get_os();
+};
